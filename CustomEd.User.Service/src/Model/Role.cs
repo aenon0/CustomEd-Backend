f45f5136d2bfddfd95481850704176fee5c6d4ac
= <amanuel.tewodros@a2sv.org>
2024-03-04 23:09:48 +0300

    Create User Class

---
+++ CustomEd.User.Service/src/Model/Role.cs
@@ -0,0 +1,11 @@
+namespace CustomEd.User.Service.Model
+{
+    public enum Role
+    {
+        Student,
+        Teacher,
+        Admin
+    }
+
+    
+}
---
+++ CustomEd.User.Service/src/Model/User.cs
@@ -0,0 +1,9 @@
+namespace CustomEd.User.Service.Model;
+
+public class User 
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public Role Role { get; set; }
+}
