896dd19dd65f1a49fe15c4c5ed56611ed2051ab7
henon solomon <henon@henons-MacBook-Air.local>
2024-03-07 16:32:25 +0300

    replace gmail verification by outlook

--- CustomEd.OtpService/Service/EmailService.cs
+++ CustomEd.OtpService/Service/EmailService.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -7,46 +6,23 @@ namespace CustomEd.OtpService.Service
 {
     public class EmailService : IEmailService
     {
-        private readonly string _senderEmailAddress = Environment.GetEnvironmentVariable("SENDER_EMAIL");
-        private readonly string  _password = Environment.GetEnvironmentVariable("SENDER_PASSWORD");
-        private SmtpClient _client;
+        private SmtpClient _smtpClient;
+        private readonly string mail = Environment.GetEnvironmentVariable("EMAIL_ADDRESS"); 
+        private readonly string password = Environment.GetEnvironmentVariable("EMAIL_PASSWORD");
 
         public EmailService()
         {
-            
-            _client = new SmtpClient("smtp-mail.outlook.com", 587)
+            _smtpClient = new SmtpClient("smtp.office365.com", 587)
             {
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(_senderEmailAddress, _password),
                 EnableSsl = true,
-                Port = 587
+                Credentials = new NetworkCredential(mail, password)
             };
+            
         }
 
-        public Task SendEmail(string recieverEmailAddress, string message)
+        public async Task SendEmail(string recieverEmailAddress, string message)
         {
-            return _client.SendMailAsync(new MailMessage(_senderEmailAddress, recieverEmailAddress, "CustomEd: OTP Code", message));
+            await _smtpClient.SendMailAsync( new MailMessage(from: mail, to: recieverEmailAddress, "CustomEd: Verification Message", message));
         }
     }
 }
-
-
-//  SmtpClient client = new SmtpClient("smtp.ethereal.email", 587);
-//             client.EnableSsl = true;
-//             client.UseDefaultCredentials = false;
-//             client.Credentials = new NetworkCredential("abby.wehner53@ethereal.email", "djgwPz8DPss78aakBv");
-
-//             // Create email message
-//             MailMessage mailMessage = new MailMessage();
-//             mailMessage.From = new MailAddress("abby.wehner53@ethereal.email");
-//             mailMessage.To.Add(toEmail);
-//             mailMessage.Subject = subject;
-//             mailMessage.IsBodyHtml = true;
-//             StringBuilder mailBody = new StringBuilder();
-//             mailBody.AppendFormat("<h1>User Registered</h1>");
-//             mailBody.AppendFormat("<br />");
-//             mailBody.AppendFormat("<p>Thank you For Registering account</p>");
-//             mailMessage.Body = mailBody.ToString();
-
-//             // Send email
-//             client.Send(mailMessage);
