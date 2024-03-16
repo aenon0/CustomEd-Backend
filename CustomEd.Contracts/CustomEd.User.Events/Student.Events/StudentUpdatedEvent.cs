a5d9ab5b401f572b5409d2be1053167b31d0566a
= <amanuel.tewodros@a2sv.org>
2024-03-16 23:11:38 +0300

    Fix Date in Events

--- CustomEd.Contracts/CustomEd.User.Events/Student.Events/StudentUpdatedEvent.cs
+++ CustomEd.Contracts/CustomEd.User.Events/Student.Events/StudentUpdatedEvent.cs
@@ -8,10 +8,10 @@ namespace CustomEd.User.Student.Events
         public string StudentId { get; set; } = null!;
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
-        public DateTime DateOfBirth { get; set; }
+        public DateOnly DateOfBirth { get; set; }
         public Department Department { get; set; }
         public string PhoneNumber { get; set; } = null!;
-        public DateTime JoinDate { get; set; }
+        public DateOnly JoinDate { get; set; }
         public int Year { get; set; }
         public string Section { get; set; } = null!;
         public string Email { get; set; } = null!;
