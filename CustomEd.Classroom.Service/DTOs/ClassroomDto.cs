c28947a8b7096284e0358219487712f13f9c912c
= <amanuel.tewodros@a2sv.org>
2024-03-16 17:08:58 +0300

    Add Classroom DTOs

--- CustomEd.Classroom.Service/DTOs/ClassroomDto.cs
+++ CustomEd.Classroom.Service/DTOs/ClassroomDto.cs
@@ -5,10 +5,11 @@ namespace CustomEd.Classroom.Service.DTOs
 {
     public class ClassroomDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; } = null!;
         public string CourseNo { get; set; } = null!;
         public string Description { get; set; } = null!;
         public Teacher Creator { get; set; } = null!;
-        public List<Student> Members {get; set;} = null!;
+        public List<Student> Members { get; set; } = null!;
     }
 }
--- CustomEd.Classroom.Service/DTOs/UpdateClassroomDto.cs
+++ CustomEd.Classroom.Service/DTOs/UpdateClassroomDto.cs
@@ -2,12 +2,12 @@ using System.Collections.Generic;
 
 namespace CustomEd.Classroom.Service.DTOs
 {
-    // public class UpdateClassroomDto
-    // {
-    //     public string Name { get; set; } = null!;
-    //     public string CourseNo { get; set; } = null!;
-    //     public string Description { get; set; } = null!;
-    //     public Teacher Creator { get; set; } = null!;
-    //     public List<Student> Members {get; set;} = null!;
-    // }
+    public class UpdateClassroomDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string CourseNo { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public Guid Creator { get; set; }
+    }
 }
