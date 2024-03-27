cc8da7ac6d203fc8ce24c70ee9fdaf7131addae8
aenon0 <aenonsolomon0@gmail.com>
2024-03-28 01:05:17 +0300

    add studentevent consumer to the notification service

--- CustomEd.Notification.Service/Consumers/StudentEventsConsumers/StudentCreatedEventConsumer.cs
+++ CustomEd.Notification.Service/Consumers/StudentEventsConsumers/StudentCreatedEventConsumer.cs
@@ -7,25 +7,24 @@ using AutoMapper;
 using CustomEd.User.Student.Events;
 
 namespace CustomEd.Notification.Service.Consumers.StudentConsumers;
-{
-    public class StudentCreatedEventConsumer : IConsumer<StudentCreatedEvent>
-    {
-        private readonly IGenericRepository<Student> _studentRepository;
-        private readonly IMapper _mapper;
 
-        public StudentCreatedEventConsumer(IGenericRepository<Student> studentRepository, IMapper mapper)
-        {
-    
-            _studentRepository = studentRepository;
-            _mapper = mapper;   
-        }
-        public async Task Consume(ConsumeContext<StudentCreatedEvent> context)
-        {
-            var StudentCreatedEvent = context.Message;
-            var Student = _mapper.Map<Student>(StudentCreatedEvent);
-            Student.Id = StudentCreatedEvent.Id; 
-            await _studentRepository.CreateAsync(Student);
-            return;
-        }
+public class StudentCreatedEventConsumer : IConsumer<StudentCreatedEvent>
+{
+    private readonly IGenericRepository<Student> _studentRepository;
+    private readonly IMapper _mapper;
+
+    public StudentCreatedEventConsumer(IGenericRepository<Student> studentRepository, IMapper mapper)
+    {
+
+        _studentRepository = studentRepository;
+        _mapper = mapper;   
+    }
+    public async Task Consume(ConsumeContext<StudentCreatedEvent> context)
+    {
+        var StudentCreatedEvent = context.Message;
+        var Student = _mapper.Map<Student>(StudentCreatedEvent);
+        Student.Id = StudentCreatedEvent.Id; 
+        await _studentRepository.CreateAsync(Student);
+        return;
     }
 }
