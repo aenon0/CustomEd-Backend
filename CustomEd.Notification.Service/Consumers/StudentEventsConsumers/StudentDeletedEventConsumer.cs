7778ac611bd0543d7c85a89b4c1e1d4fbf67f6e0
aenon0 <aenonsolomon0@gmail.com>
2024-03-28 01:05:17 +0300

    add studentevent consumer to the notification service

--- CustomEd.Notification.Service/Consumers/StudentEventsConsumers/StudentDeletedEventConsumer.cs
+++ CustomEd.Notification.Service/Consumers/StudentEventsConsumers/StudentDeletedEventConsumer.cs
@@ -6,23 +6,22 @@ using AutoMapper;
 using CustomEd.User.Student.Events;
 
 namespace CustomEd.Notification.Service.Consumers.StudentConsumers;
-{
-    public class StudentDeletedEventConsumer : IConsumer<StudentDeletedEvent>
-    {
-        private readonly IGenericRepository<Student> _studentRepository;
-        private readonly IMapper _mapper;
 
-        public StudentDeletedEventConsumer(IGenericRepository<Student> studentRepository, IMapper mapper)
-        {
-    
-            _studentRepository = studentRepository;
-            _mapper = mapper;   
-        }
-        public async Task Consume(ConsumeContext<StudentDeletedEvent> context)
-        {
-            var StudentDeletedEvent = context.Message; 
-            await _studentRepository.RemoveAsync(StudentDeletedEvent.Id);
-            return;
-        }
+public class StudentDeletedEventConsumer : IConsumer<StudentDeletedEvent>
+{
+    private readonly IGenericRepository<Student> _studentRepository;
+    private readonly IMapper _mapper;
+
+    public StudentDeletedEventConsumer(IGenericRepository<Student> studentRepository, IMapper mapper)
+    {
+
+        _studentRepository = studentRepository;
+        _mapper = mapper;   
+    }
+    public async Task Consume(ConsumeContext<StudentDeletedEvent> context)
+    {
+        var StudentDeletedEvent = context.Message; 
+        await _studentRepository.RemoveAsync(StudentDeletedEvent.Id);
+        return;
     }
 }
