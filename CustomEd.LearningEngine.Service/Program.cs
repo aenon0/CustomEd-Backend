cf8cfa16a49c90f7e775f7108ef1acc076c5d8da
Henon Solomon <95585844+aenon0@users.noreply.github.com>
2024-04-01 21:25:24 +0300

    add consumer to the learning engine service

--- CustomEd.LearningEngine.Service/Program.cs
+++ CustomEd.LearningEngine.Service/Program.cs
@@ -1,6 +1,36 @@
+using CustomEd.LearningEngine.Service.Model;
+using CustomEd.Shared.Data;
+using CustomEd.Shared.JWT;
+using CustomEd.Shared.JWT.Interfaces;
+using CustomEd.Shared.RabbitMQ;
+using CustomEd.Shared.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddMongo();
+builder.Services.AddPersistence<Student>("Student");
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMassTransitWithRabbitMq();
+builder.Services.AddAuth();
+builder.Services.AddSingleton<IJwtService, JwtService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IdentityProvider>();
+
+// builder.Services.AddAuthorization(options =>
+// {
+//     options.AddPolicy("TeacherOnlyPolicy", policy =>
+//         {
+//             policy.RequireAuthenticatedUser();
+//             policy.RequireClaim(ClaimTypes.Role, CustomEd.Shared.Model.Role.Teacher.ToString());
+        
+//         });
+// });
+
+builder.Services.AddControllers();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
