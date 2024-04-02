16af5cb92e87b540486c0345345138c56540d9f7
Henon Solomon <95585844+aenon0@users.noreply.github.com>
2024-04-02 23:27:03 +0300

    createmodels, policy, configure program.cs

--- CustomEd.Discussion.Service/Program.cs
+++ CustomEd.Discussion.Service/Program.cs
@@ -1,19 +1,39 @@
+using CustomEd.Discussion.Service.Model;
+using CustomEd.Discussion.Service.Policies;
+using CustomEd.Shared.Data;
+using CustomEd.Shared.JWT;
+using CustomEd.Shared.JWT.Interfaces;
+using CustomEd.Shared.Settings;
+using Microsoft.AspNetCore.Authorization;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddMongo()
+                .AddPersistence<Discussion>("Classroom")
+                .AddPersistence<Discussion>("Discussion");
+
+                
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IJwtService, JwtService>();
+builder.Services.AddAuth();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MemberOnlyPolicy", policy =>
+        policy.Requirements.Add(new MemberOnlyRequirement()));
+});
+builder.Services.AddScoped<IAuthorizationHandler, MemberOnlyPolicy>();
 
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
