6de4e7ea0e77c5c6f0313d699abb6bca23c3457c
Henon Solomon <95585844+aenon0@users.noreply.github.com>
2024-04-09 19:01:09 +0300

    ammend model-naming

--- CustomEd.LearningEngine.Service/Model/LearningPath.cs
+++ CustomEd.LearningEngine.Service/Model/LearningPath.cs
@@ -7,7 +7,7 @@ namespace CustomEd.LearningEngine.Service.Model
     public class LearningPath : BaseEntity
     {
         public Guid StudentId { set; get; }
-        public string Path {set; get;}
+        public string Content {set; get;}
         public LearningPathStatus Status {set; get;}
         
     }
