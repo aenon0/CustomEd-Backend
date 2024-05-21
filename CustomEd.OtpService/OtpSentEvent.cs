e4dbce0cfc549c028dea3ea47434b8ae4e29e5f4
Henon Solomon <95585844+aenon0@users.noreply.github.com>
2024-05-21 10:25:54 +0300

    remove some files

--- CustomEd.OtpService/OtpSentEvent.cs
+++
@@ -1,8 +0,0 @@
-namespace CustomEd.Contracts.OtpService.Events;
-
-public class OtpSentEvent
-{
-    public Guid Id {set; get;}
-    public string Email {set; get;}
-    public string OtpCode {set; get;}
-}
